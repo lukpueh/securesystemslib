bdc10d5dd0593ac73d0537b6516901fb22523576
Lukas Puehringer <lukas.puehringer@nyu.edu>
2020-03-25 16:38:55 +0100

    Revise HSM (WIP) [ci skip]

--- securesystemslib/hsm.py
+++ securesystemslib/hsm.py
@@ -25,6 +25,7 @@ import logging
 import binascii
 import asn1crypto
 
+import securesystemslib.formats
 from securesystemslib.exceptions import (
     UnsupportedLibraryError, PKCS11DynamicLibraryLoadingError)
 
@@ -60,7 +61,6 @@ PKCS11_DYN_LIB = False
 NO_PKCS11_PY_LIB_MSG = "HSM support requires PyKCS11 library"
 NO_PKCS11_DYN_LIB_MSG = "HSM support requires PKCS11 shared object"
 
-
 # Import python wrapper for PKCS#11 to communicate with the tokens
 try:
   import PyKCS11
@@ -88,12 +88,25 @@ try:
     "ecdsa-sign": ECDSA_SIGN
   }
 
+
+  def _load_pkcs11_lib(path=None):
+    """Define this  """
+    global PKCS11_DYN_LIB
+    try:
+      # If path is not passed PyKCS11 consults the PYKCS11LIB env var
+      PKCS11.load(path)
+      PKCS11_DYN_LIB = True
+
+    # TODO: Should we catch any exception
+    except PyKCS11.PyKCS11Error as e:
+      PKCS11_DYN_LIB = False
+      # TODO: Add message "Could not load + e + NO_PKCS11_DYN_LIB_MSG
+      raise PKCS11DynamicLibraryLoadingError(e)
+
   # Try loading PKCS#11 dynamic library from the path at PYKCS11LIB envvar
   # TODO: We do this to spare the user, who has PYKCS11LIB set, some state
   # handling. Is this too much magic? Should we require an explicit call?
-  load_pkcs11_library()
-
-
+  _load_pkcs11_lib()
 
 except ImportError as e:
   # Missing PyKCS11 python library. PKCS11 must remain 'None'.
@@ -128,26 +141,18 @@ def load_pkcs11_lib(path=None):
 
   """
   if PKCS11 is None:
-    raise UnsupportedLibraryError(
-        NO_PKCS11_PY_LIB_MSG)
+    raise UnsupportedLibraryError(NO_PKCS11_PY_LIB_MSG)
 
-  global PKCS11_DYN_LIB
-  try:
-    # If path is not passed PyKCS11 consults the PYKCS11LIB env var
-    PKCS11.load(path)
-    PKCS11_DYN_LIB = True
+  if path is not None:
+    securesystemslib.formats.PATH_SCHEMA.check_match(path)
 
-  # TODO: Should we catch any exception
-  except PyKCS11.PyKCS11Error as e:
-    PKCS11_DYN_LIB = False
-    # TODO: Add message "Could not load + e + NO_PKCS11_DYN_LIB_MSG
-    raise PKCS11DynamicLibraryLoadingError(e)
+  _load_pkcs11_lib(path)
 
 
-def get_smartcards():
+def get_hsms():
   """
   <Purpose>
-    Iterate over smart card slots and return list with info for each HSM.
+    Iterate over hsm slots and return list with info for each HSM.
 
   <Return>
     List of dictionaries conforming to HSM_INFO_SCHEMA.
@@ -160,24 +165,23 @@ def get_smartcards():
   if not PKCS11_DYN_LIB:
     raise UnsupportedLibraryError(NO_PKCS11_DYN_LIB_MSG)
 
-  slot_list = PKCS11.getSlotList()
-
-  for slot in slot_list:
+  hsm_info_list = []
+  for slot in PKCS11.getSlotList():
     slot_info = PKCS11.getSlotInfo(slot)
     hsm_info_list.append({
         "slot_id": slot,
         "slot_description": slot_info.slotDescription.strip(),
-        "manufacturer_id": hsm_info.manufacturerID.strip(),
-        "hardware_version": hsm_info.hardwareVersion,
-        "firmware_version": hsm_info.firmwareVersion,
-        "flags": hsm_info.flags2text(),
+        "manufacturer_id": slot_info.manufacturerID.strip(),
+        "hardware_version": slot_info.hardwareVersion,
+        "firmware_version": slot_info.firmwareVersion,
+        "flags": slot_info.flags2text(),
       })
 
   return hsm_info_list
 
 
 
-def get_hsm_keys(hsm_info, user_pin=None):
+def get_keys_on_hsm(hsm_info, user_pin=None):
   """
   <Purpose>
     Get handles of public and private keys stored on the HSM.
@@ -201,69 +205,37 @@ def get_hsm_keys(hsm_info, user_pin=None):
     raise UnsupportedLibraryError(NO_PKCS11_DYN_LIB_MSG)
 
   # TODO: HSM_INFO_SCHEMA.check_macht(hsm_info)
-  securesystemslib.format.PASSWORD_SCHEMA.check(user_ping)
+  if user_pin:
+    securesystemslib.formats.PASSWORD_SCHEMA.check_match(user_pin)
 
-  # Create an HSM session and login to access private objects.
-  session = _setup_session.
+  # Create an HSM session and, if pin passed, login to access private objects
+  session = _setup_session(hsm_info, user_pin)
 
-  if user_pin not None:
-    _login(session, user_pin)
+  hsm_key_info_list = []
+  # Iterate over public and private (if logged in) keys and construct key info
+  for obj in session.findObjects(
+      [(PyKCS11.CKA_CLASS, PyKCS11.CKO_PRIVATE_KEY)]) + session.findObjects(
+      [(PyKCS11.CKA_CLASS, PyKCS11.CKO_PUBLIC_KEY)]):
 
+    # TODO: Re-think data structure: attribute names and values should be human
+    # readable but also keep the PKCS11 constants
+    hsm_key_info_list.append({
+        "_id": session.getAttributeValue(obj, [PyKCS11.CKA_ID]),
+        "_class": session.getAttributeValue(obj, [PyKCS11.CKA_CLASS]),
+        "_type": session.getAttributeValue(obj, [PyKCS11.CKA_KEY_TYPE]),
+        "label": session.getAttributeValue(obj, [PyKCS11.CKA_LABEL]),
+        "sign": session.getAttributeValue(obj, [PyKCS11.CKA_SIGN]),
+      })
 
+  # Logout, if logged in, and close session
+  _teardown_session(session)
 
-
-
-
-  # Retirve all the private key object present on the HSM
-  private_key_objects = session.findObjects([(PyKCS11.CKA_CLASS,
-      PyKCS11.CKO_PRIVATE_KEY)])
-
-  # TODO: Find a better way to provide the details regarding the available keys.
-  key_info = []
-  for object_handle in private_key_objects:
-    # Find and return the key_id(with resepect to the HSM) and the key modulus
-    key_id, key_modulus = session.getAttributeValue(object_handle,
-        [PyKCS11.CKA_ID, PyKCS11.CKA_MODULUS])
-    key_info.append([key_id, key_modulus])
-
-  return key_info
-
-
+  return hsm_key_info_list
 
 
-def get_public_key_objects(hsm_info):
-  """
-  <Purpose>
-    Get object handles of public keys stored on the HSM.
-
-  <Returns>
-    List of  key_id and key_modulus for all the public keys in HSM
-  """
-  if not HSM_SUPPORT: # pragma: no cover
-    raise securesystemslib.exceptions.UnsupportedLibraryError(NO_HSM_MSG)
-
-  if not HSM_LIB: # pragma: no cover
-    raise securesystemslib.exceptions.UnsupportedLibraryError(NO_HSM_LIB_MSG)
-
-  # Create an HSM session to access private objects.
-  session = _create_session(hsm_info)
-
-  # Retirve all the public key object present on the HSM.
-  public_key_objects = session.findObjects([(PyKCS11.CKA_CLASS,
-      PyKCS11.CKO_PUBLIC_KEY)])
-
-  # TODO: Find a better way to provide the details regarding the available keys.
-  key_info = []
-  for object_handle in public_key_objects:
-    # Find and return the key_id(with resepect to the HSM) and the key modulus
-    key_id, key_modulus = session.getAttributeValue(object_handle,
-        [PyKCS11.CKA_ID, PyKCS11.CKA_MODULUS])
-    key_info.append([key_id, key_modulus])
-
-  return key_info
 
 
-# def export_pubkey(hsm_info, public_key_info):
+def export_pubkey(hsm_info, public_key_info):
 #   """
 #   <Purpose>
 #     Get the public key value corresponding to the 'public_key_handle'
@@ -353,79 +325,79 @@ def get_public_key_objects(hsm_info):
 #   return key_dict
 
 
-# def create_signature(data, hsm_info, private_key_info, user_pin):
-#   """
-#   <Purpose>
-#     Calculate signature over 'data' using the private key corresponding
-#     to the 'private_key_info'
+# # def create_signature(data, hsm_info, private_key_info, user_pin):
+# #   """
+# #   <Purpose>
+# #     Calculate signature over 'data' using the private key corresponding
+# #     to the 'private_key_info'
 
-#     Supported Keys
-#     1. RSA - rsassa-pss-sha256
-#     2. ECDSA
+# #     Supported Keys
+# #     1. RSA - rsassa-pss-sha256
+# #     2. ECDSA
 
-#   <Arguments>
-#     data:
-#       bytes over which the signature is to be calculated
-#       'data' should be encoded/serialized before it is passed here.
+# #   <Arguments>
+# #     data:
+# #       bytes over which the signature is to be calculated
+# #       'data' should be encoded/serialized before it is passed here.
 
-#     private_key_info:
-#       element from the list returned by the get_private_key_objects()
+# #     private_key_info:
+# #       element from the list returned by the get_private_key_objects()
 
-#     user_pin:
-#       PIN for the CKU_USER login.
+# #     user_pin:
+# #       PIN for the CKU_USER login.
 
-#   <Exceptions>
-#     securesystemslib.exceptions.UnsupportedAlgorithmError, when the
-#     key type of the 'private_key_handle' is not supported
+# #   <Exceptions>
+# #     securesystemslib.exceptions.UnsupportedAlgorithmError, when the
+# #     key type of the 'private_key_handle' is not supported
 
-#   <Returns>
-#     A signature dictionary conformant to
-#     'securesystemslib_format.SIGNATURE_SCHEMA'.
-#   """
+# #   <Returns>
+# #     A signature dictionary conformant to
+# #     'securesystemslib_format.SIGNATURE_SCHEMA'.
+# #   """
 
-#   if not HSM_SUPPORT: # pragma: no cover
-#     raise securesystemslib.exceptions.UnsupportedLibraryError(NO_HSM_MSG)
+# #   if not HSM_SUPPORT: # pragma: no cover
+# #     raise securesystemslib.exceptions.UnsupportedLibraryError(NO_HSM_MSG)
 
-#   if not HSM_LIB: # pragma: no cover
-#     raise securesystemslib.exceptions.UnsupportedLibraryError(NO_HSM_LIB_MSG)
+# #   if not HSM_LIB: # pragma: no cover
+# #     raise securesystemslib.exceptions.UnsupportedLibraryError(NO_HSM_LIB_MSG)
 
-#   # Create a session and login to generate signature using keys stored in hsm
-#   session = _create_session(hsm_info)
-#   _login(session, str(user_pin))
+# #   # Create a session and login to generate signature using keys stored in hsm
+# #   session = _create_session(hsm_info)
+# #   _login(session, str(user_pin))
 
-#   mechanism = None
-#   private_key_object = session.findObjects([(PyKCS11.CKA_CLASS,
-#       PyKCS11.CKO_PRIVATE_KEY), (PyKCS11.CKA_ID, private_key_info[0])])[0]
-#   key_type = session.getAttributeValue(private_key_object,
-#       [PyKCS11.CKA_KEY_TYPE])[0]
+# #   mechanism = None
+# #   private_key_object = session.findObjects([(PyKCS11.CKA_CLASS,
+# #       PyKCS11.CKO_PRIVATE_KEY), (PyKCS11.CKA_ID, private_key_info[0])])[0]
+# #   key_type = session.getAttributeValue(private_key_object,
+# #       [PyKCS11.CKA_KEY_TYPE])[0]
 
-#   if PyKCS11.CKK[key_type] == 'CKK_RSA':
-#     mechanism = MECHANISMS["rsa-pkcs1v15-sha256"]
+# #   if PyKCS11.CKK[key_type] == 'CKK_RSA':
+# #     mechanism = MECHANISMS["rsa-pkcs1v15-sha256"]
 
-#   elif PyKCS11.CKK[key_type] == 'CKK_EC' or PyKCS11.CKK[key_type] == 'CKK_ECDSA':
-#     mechanism = MECHANISMS["ecdsa-sign"]
+# #   elif PyKCS11.CKK[key_type] == 'CKK_EC' or PyKCS11.CKK[key_type] == 'CKK_ECDSA':
+# #     mechanism = MECHANISMS["ecdsa-sign"]
 
-#   if mechanism is None:
-#     raise securesystemslib.exceptions.UnsupportedAlgorithmError(
-#       "The Key type " + repr(key_type) + " is currently not supported!")
-
-
-#   signature = session.sign(private_key_object, data, mechanism)
-
-#   signature_dict = {}
-#   # TODO: This is not a key id, change this.
-#   keyid = _to_hex(private_key_info[0])
-#   sig = _to_hex(signature)
-
-#   signature_dict['keyid'] = keyid
-#   signature_dict['sig'] = sig
-
-#   return signature_dict
+# #   if mechanism is None:
+# #     raise securesystemslib.exceptions.UnsupportedAlgorithmError(
+# #       "The Key type " + repr(key_type) + " is currently not supported!")
 
 
+# #   signature = session.sign(private_key_object, data, mechanism)
+
+# #   signature_dict = {}
+# #   # TODO: This is not a key id, change this.
+# #   keyid = _to_hex(private_key_info[0])
+# #   sig = _to_hex(signature)
+
+# #   signature_dict['keyid'] = keyid
+# #   signature_dict['sig'] = sig
+
+# #   return signature_dict
 
 
-def _setup_session(hsm_info, pin=None):
+
+
+def _setup_session(hsm_info, user_pin=None):
   """Create new hsm session, login if pin is passed and return session object.
   """
   try:
@@ -433,7 +405,7 @@ def _setup_session(hsm_info, pin=None):
         hsm_info["slot_id"],
         PyKCS11.CKF_SERIAL_SESSION | PyKCS11.CKF_RW_SESSION)
 
-    if pin not None:
+    if user_pin is not None:
       session.login(user_pin)
 
   except PyKCS11.PyKCS11Error as e:
@@ -456,4 +428,7 @@ def _teardown_session(session):
       _teardown_func()
 
     except Exception as e:
-      log.debug(e)
+      logger.debug(e)
+
+if __name__ == "__main__":
+  main()
