11576ff73b7356922e7eec79bdad4889d63c1e76
Lukas Puehringer <lukas.puehringer@nyu.edu>
2020-04-03 16:09:43 +0200

    Clean up tests

--- tests/test_hsm.py
+++ tests/test_hsm.py
@@ -7,59 +7,51 @@
   Tansihq Jasoria
 
 <Purpose>
-  Test cases for hsm.py module
-
-
-  Requires PYKCS11LIB  
-
-  "/usr/local/lib/softhsm/libsofthsm2.so"
+  Test cases for hsm.py module.
 
 """
-import unittest
-import logging
 
 import os
-import shutil
-import tempfile
 import six
+import shutil
+import unittest
+import tempfile
+
+
+# We neither have these dependencies nor run the tests (see skipIf) on PY2
+if not six.PY2:
+  import PyKCS11
+  from asn1crypto.keys import ECDomainParameters, NamedCurve
 
 import securesystemslib.exceptions
 import securesystemslib.formats
 import securesystemslib.keys
 import securesystemslib.hash
-from asn1crypto.keys import ECDomainParameters, NamedCurve
-
-import PyKCS11
 import securesystemslib.hsm
+from securesystemslib.hsm import (ECDSA_SHA2_NISTP256, ECDSA_SHA2_NISTP384)
 
-logger = logging.getLogger(__name__)
-
-
-# Skip if not crypto or not pykcs11 or not Python 3
-
+# TODO: remove
 os.environ["PYKCS11LIB"] = "/usr/local/lib/softhsm/libsofthsm2.so"
 
+@unittest.skipIf(six.PY2, "HSM interface not supported on Python 2")
 class SoftHSMTestCase(unittest.TestCase):
   """
-  I prefer an approach that uses a temporary directory, something like:
-  in setupClass
+  Class to load PKCS11 dynamic library, set up SoftHSM in a temporary
+  directory and initialize security officer (so_pin) and user pin (user_pin).
+  Subclasses may use the class variable 'hsm_info' to identify the HSM.
 
-  backup original cwd
-  create and change into temporary directory
-  create a tokens directory
-  create a softhsm2.conf file with a line directories.tokendir = /path/to/tmpdir/tokens/
-  init softhsm
-  in tearDownClass
+  IMPORTANT:
+  Requires the environment variable 'PYKCS11LIB' to point to the SoftHSM
+  shared library, e.g.:
+      /usr/local/lib/softhsm/libsofthsm2.so
 
-  deinit softhsm
-  change back to original cwd
-  remove temporary directory
+  See https://github.com/opendnssec/SoftHSMv2 or your favorite system package
+  manager for installation details.
 
   """
-  user_pin = "123456"
   so_pin = "654321"
-  hsm_label = "Virtual Test HSM"
-  test_hsm = None
+  user_pin = "123456"
+  hsm_info = None
 
 
   @classmethod
@@ -73,11 +65,12 @@ class SoftHSMTestCase(unittest.TestCase):
 
     os.environ["SOFTHSM2_CONF"] = os.path.join(cls.test_dir, "softhsm2.conf")
 
-    # Initializing the HSM (load library via envvar)
+    # Initializing the HSM
+    # make sure to set PYKCS11LIB!!
     securesystemslib.hsm.load_pkcs11_lib()
     available_hsm = securesystemslib.hsm.get_hsms().pop()
     securesystemslib.hsm.PKCS11.initToken(
-        available_hsm["slot_id"], cls.so_pin, cls.hsm_label)
+        available_hsm["slot_id"], cls.so_pin, "Test HSM (SoftHSM) Label")
 
     # After initializing the SoftHSM, the slot number changes (get_hsms again)
     cls.hsm_info = securesystemslib.hsm.get_hsms().pop()
@@ -96,76 +89,100 @@ class SoftHSMTestCase(unittest.TestCase):
 
 
 class TestECDSA(SoftHSMTestCase):
+  """Generate EC key pairs on SoftHSM for to test ECDSA signing. """
+
+  @classmethod
+  def _generate_key_pair(cls, session, hsm_key_id, scheme):
+    """Using the passed PKCS11 HSM 'session', generate an elliptic curve key
+    pair under 'hsm_key_id' on the curve we use for the passed 'scheme'.
+
+    """
+    # Get curve name corresponding to the passed scheme and encode it as
+    # "domain parameters".
+    curve = securesystemslib.hsm.SIGNING_SCHEMES[scheme]["curve"].name
+    params = ECDomainParameters(name="named", value=NamedCurve(curve)).dump()
+
+    # Define PyKCS11 templates for elliptic curve public private key pairs ...
+    ec_public_template = [
+        (PyKCS11.CKA_CLASS, PyKCS11.CKO_PUBLIC_KEY),
+        (PyKCS11.CKA_PRIVATE, PyKCS11.CK_FALSE),
+        (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_ENCRYPT, PyKCS11.CK_FALSE),
+        (PyKCS11.CKA_VERIFY, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_WRAP, PyKCS11.CK_FALSE),
+        (PyKCS11.CKA_KEY_TYPE, PyKCS11.CKK_ECDSA),
+        (PyKCS11.CKA_EC_PARAMS, params),
+        (PyKCS11.CKA_LABEL, curve),
+        (PyKCS11.CKA_ID, hsm_key_id),
+      ]
+    ec_private_template = [
+        (PyKCS11.CKA_CLASS, PyKCS11.CKO_PRIVATE_KEY),
+        (PyKCS11.CKA_KEY_TYPE, PyKCS11.CKK_ECDSA),
+        (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_SENSITIVE, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_DECRYPT, PyKCS11.CK_FALSE),
+        (PyKCS11.CKA_SIGN, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_UNWRAP, PyKCS11.CK_FALSE),
+        (PyKCS11.CKA_LABEL, curve),
+        (PyKCS11.CKA_ID, hsm_key_id),
+      ]
+
+    # ... and generate it on the virtual HSM (SoftHSM).
+    public_key, private_key = session.generateKeyPair(
+        ec_public_template, ec_private_template,
+        mecha=PyKCS11.MechanismECGENERATEKEYPAIR)
+
+
+
   @classmethod
   def setUpClass(cls):
     super(TestECDSA, cls).setUpClass()
 
+    # Define hsm_key_ids and schemes for the keys we want to test with
+    cls.hsm_keyid_scheme_tuples = [
+      ((0x00, ), ECDSA_SHA2_NISTP256),
+      ((0x01, ), ECDSA_SHA2_NISTP384)
+    ]
+    # Define backup dict for schemes (see usage below)
+    cls.original_mechanisms = {}
+
+    # Setup session on the SoftHSM token (see parent class)
     session = securesystemslib.hsm._setup_session(cls.hsm_info, cls.user_pin)
 
-    # Patch mechanism securesystemslib signing schemes
-    securesystemslib.hsm.SIGNING_SCHEMES[securesystemslib.hsm.ECDSA_SHA2_NISTP256]["mechanism"] = \
-        PyKCS11.Mechanism(PyKCS11.CKM_ECDSA)
-    securesystemslib.hsm.SIGNING_SCHEMES[securesystemslib.hsm.ECDSA_SHA2_NISTP384]["mechanism"] = \
-        PyKCS11.Mechanism(PyKCS11.CKM_ECDSA)
+    for hsm_key_id, scheme in cls.hsm_keyid_scheme_tuples:
+      # Generate test key pairs
+      cls._generate_key_pair(session, hsm_key_id, scheme)
 
-    cls.key_infos = [
-      ((0x00, ), "secp256r1", securesystemslib.hsm.ECDSA_SHA2_NISTP256),
-      ((0x01, ), "secp384r1", securesystemslib.hsm.ECDSA_SHA2_NISTP384)
-    ]
-
-    for hsm_key_id, curve, _ in cls.key_infos:
-      domain_params = ECDomainParameters(name="named", value=NamedCurve(curve))
-      ec_params = domain_params.dump()
-
-      ec_public_template = [
-          (PyKCS11.CKA_CLASS, PyKCS11.CKO_PUBLIC_KEY),
-          (PyKCS11.CKA_PRIVATE, PyKCS11.CK_FALSE),
-          (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
-          (PyKCS11.CKA_ENCRYPT, PyKCS11.CK_FALSE),
-          (PyKCS11.CKA_VERIFY, PyKCS11.CK_TRUE),
-          (PyKCS11.CKA_WRAP, PyKCS11.CK_FALSE),
-          (PyKCS11.CKA_KEY_TYPE, PyKCS11.CKK_ECDSA),
-          (PyKCS11.CKA_EC_PARAMS, ec_params),
-          (PyKCS11.CKA_LABEL, curve),
-          (PyKCS11.CKA_ID, hsm_key_id),
-      ]
-      ec_private_template = [
-          (PyKCS11.CKA_CLASS, PyKCS11.CKO_PRIVATE_KEY),
-          (PyKCS11.CKA_KEY_TYPE, PyKCS11.CKK_ECDSA),
-          (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
-          (PyKCS11.CKA_SENSITIVE, PyKCS11.CK_TRUE),
-          (PyKCS11.CKA_DECRYPT, PyKCS11.CK_FALSE),
-          (PyKCS11.CKA_SIGN, PyKCS11.CK_TRUE),
-          (PyKCS11.CKA_UNWRAP, PyKCS11.CK_FALSE),
-          (PyKCS11.CKA_LABEL, curve),
-          (PyKCS11.CKA_ID, hsm_key_id),
-      ]
-
-      public_key, private_key = session.generateKeyPair(
-          ec_public_template, ec_private_template,
-          mecha=PyKCS11.MechanismECGENERATEKEYPAIR)
+      # Back up and monkey-patch signing mechanism for testing with SoftHSM
+      # NOTE: SoftHSM only support the unhashed CKM_ECDSA mechanism, to still
+      # be able to test 'securesystemslib.hsm' we monkey-patch its supported
+      # mechanisms and pre-hash the data in the tests below.
+      cls.original_mechanisms[scheme] = \
+          securesystemslib.hsm.SIGNING_SCHEMES[scheme]["mechanism"]
+      securesystemslib.hsm.SIGNING_SCHEMES[scheme]["mechanism"] = \
+          PyKCS11.Mechanism(PyKCS11.CKM_ECDSA)
 
     securesystemslib.hsm._teardown_session(session)
+
+
 
   @classmethod
   def tearDownClass(cls):
     super(TestECDSA, cls).tearDownClass()
 
-    # Restore mechanism for TestECDSAOnYubiKey to work
-    securesystemslib.hsm.SIGNING_SCHEMES[securesystemslib.hsm.ECDSA_SHA2_NISTP256]["mechanism"] = \
-        PyKCS11.Mechanism(PyKCS11.CKM_ECDSA_SHA256)
-    securesystemslib.hsm.SIGNING_SCHEMES[securesystemslib.hsm.ECDSA_SHA2_NISTP384]["mechanism"] = \
-        PyKCS11.Mechanism(PyKCS11.CKM_ECDSA_SHA384)
+    # Restore signing mechanisms that were monkey-patched in setUpClass
+    for scheme, mechanism in cls.original_mechanisms.items():
+      securesystemslib.hsm.SIGNING_SCHEMES[scheme]["mechanism"] = mechanism
+
+
 
   def test_keys(self):
     sslib_key_id = "123456"
     data = b"Hello world"
 
-    for hsm_key_id, _, scheme in self.key_infos:
+    for hsm_key_id, scheme in self.hsm_keyid_scheme_tuples:
       public_key = securesystemslib.hsm.export_pubkey(
           self.hsm_info, hsm_key_id, scheme, sslib_key_id)
-      print(public_key)
-
       # Create a signature
       # We have to prehash the data for SoftHSM, which only supports CKM_ECDSA
       # mechanism
@@ -182,10 +199,12 @@ class TestECDSA(SoftHSMTestCase):
 
 
 
-class TestECDSAOnYubiKey(unittest.TestCase):
+# TODO: Remove here, add as example usage in README.md
+@unittest.skipUnless(os.environ.get("LUKPUEH_YUBI_PIN", None), "tmp local testing")
+class TestECDSAOnLUKPUEHsYubiKey(unittest.TestCase):
   @classmethod
   def setUpClass(cls):
-    cls.user_pin = os.environ["YUBI_PIN"]
+    cls.user_pin = os.environ["LUKPUEH_YUBI_PIN"]
 
     securesystemslib.hsm.load_pkcs11_lib(
         "/usr/local/Cellar/yubico-piv-tool/2.0.0/lib/libykcs11.dylib")
@@ -193,8 +212,8 @@ class TestECDSAOnYubiKey(unittest.TestCase):
     cls.sslib_key_id = "123456"
     cls.data = b"Hello world"
 
-  def test_keys(self):
-    scheme = "ecdsa-sha2-nistp256"
+  def test_key(self):
+    scheme = ECDSA_SHA2_NISTP256
     hsm_key_id = (0x02, )
 
     public_key = securesystemslib.hsm.export_pubkey(
