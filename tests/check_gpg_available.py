3c69f25e4bf2a2fbd655a802e9e14f33f89f9c67
Lukas Puehringer <lukas.puehringer@nyu.edu>
2022-10-17 12:36:51 +0200

    black

--- tests/check_gpg_available.py
+++ tests/check_gpg_available.py
@@ -32,11 +32,12 @@ import securesystemslib.gpg.constants
 
 
 class TestGpgAvailable(unittest.TestCase):
-  """Test that securesystemslib finds some GPG executable in the environment."""
+    """Test that securesystemslib finds some GPG executable in the environment."""
 
-  def test_gpg_available(self):
-    """Test that GPG is available."""
-    self.assertTrue(securesystemslib.gpg.constants.HAVE_GPG)
+    def test_gpg_available(self):
+        """Test that GPG is available."""
+        self.assertTrue(securesystemslib.gpg.constants.HAVE_GPG)
+
 
 if __name__ == "__main__":
-  unittest.main(verbosity=1, buffer=True)
+    unittest.main(verbosity=1, buffer=True)
