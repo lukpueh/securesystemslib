4563607784dd634c6d9ad5995cfe0562cf3612cd
Lukas Puehringer <lukas.puehringer@nyu.edu>
2022-10-17 12:57:30 +0200

    W0107: Unnecessary pass statement (unnecessary-pass)

--- securesystemslib/exceptions.py
+++ securesystemslib/exceptions.py
@@ -21,19 +21,13 @@
 class Error(Exception):
     """Indicate a generic error."""
 
-    pass
-
 
 class Warning(Warning):
     """Generic warning.  It is used by the 'warnings' module."""
 
-    pass
-
 
 class FormatError(Error):
     """Indicate an error while validating an object's format."""
-
-    pass
 
 
 class InvalidMetadataJSONError(FormatError):
@@ -50,8 +44,6 @@ class InvalidMetadataJSONError(FormatError):
 
 class UnsupportedAlgorithmError(Error):
     """Indicate an error while trying to identify a user-specified algorithm."""
-
-    pass
 
 
 class BadHashError(Error):
@@ -74,13 +66,9 @@ class BadHashError(Error):
 class BadPasswordError(Error):
     """Indicate an error after encountering an invalid password."""
 
-    pass
-
 
 class CryptoError(Error):
     """Indicate any cryptography-related errors."""
-
-    pass
 
 
 class BadSignatureError(CryptoError):
@@ -96,41 +84,27 @@ class BadSignatureError(CryptoError):
 class UnknownMethodError(CryptoError):
     """Indicate that a user-specified cryptograpthic method is unknown."""
 
-    pass
-
 
 class UnsupportedLibraryError(Error):
     """Indicate that a supported library could not be located or imported."""
-
-    pass
 
 
 class InvalidNameError(Error):
     """Indicate an error while trying to validate any type of named object."""
 
-    pass
-
 
 class NotFoundError(Error):
     """If a required configuration or resource is not found."""
-
-    pass
 
 
 class URLMatchesNoPatternError(Error):
     """If a URL does not match a user-specified regular expression."""
 
-    pass
-
 
 class InvalidConfigurationError(Error):
     """If a configuration object does not match the expected format."""
-
-    pass
 
 
 class StorageError(Error):
     """Indicate an error occured during interaction with an abstracted storage
     backend."""
-
-    pass
