eb63de922cb81ce5186b13461b330e517da6f5f6
Lukas Puehringer <lukas.puehringer@nyu.edu>
2023-03-14 17:04:16 +0100

    debug sigstore sign resource warning
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>

--- tests/check_sigstore_signer.py
+++ tests/check_sigstore_signer.py
@@ -5,6 +5,9 @@ NOTE: The filename prefix is check_ instead of test_ so that tests are
 only run when explicitly invoked in a suited environment.
 
 """
+import warnings
+warnings.filterwarnings("error")
+
 import os
 import unittest
 
