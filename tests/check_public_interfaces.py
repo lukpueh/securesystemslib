8b2e8a01a4d9283b672cb8e45705ec4466192ea9
Lukas Puehringer <lukas.puehringer@nyu.edu>
2023-05-30 17:30:38 +0200

    Drop legacy signature creation code
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>

--- tests/check_public_interfaces.py
+++ tests/check_public_interfaces.py
@@ -197,30 +197,9 @@ class TestPublicInterfaces(
 
         data = "foo"
         keydict = {
-            "keytype": "ed25519",
-            "scheme": "ed25519",
             "keyid": "f00",
             "keyval": {"private": "f001", "public": "b00f"},
         }
-        with self.assertRaises(
-            securesystemslib.exceptions.UnsupportedLibraryError
-        ):
-            securesystemslib.keys.create_signature(keydict, data)
-
-        keydict["keytype"] = "ecdsa"
-        keydict["scheme"] = "ecdsa-sha2-nistp256"
-        with self.assertRaises(
-            securesystemslib.exceptions.UnsupportedLibraryError
-        ):
-            securesystemslib.keys.create_signature(keydict, data)
-
-        keydict["keytype"] = "rsa"
-        keydict["scheme"] = "rsassa-pss-sha256"
-        with self.assertRaises(
-            securesystemslib.exceptions.UnsupportedLibraryError
-        ):
-            securesystemslib.keys.create_signature(keydict, data)
-
         keydict["keytype"] = "ecdsa"
         keydict["scheme"] = "ecdsa-sha2-nistp256"
         sig = {
